a6ed453a92b21b38ef156658a6d542e456dffe5b
minjeong-dev <vitaminjeong1026@gmail.com>
2022-03-10 05:22:22 +0900

    Initial Commit

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme:DefaultTheme = {
+    bgColor:"#2c3e50",
+    textColor:"black",
+    accentColor:"#1e90ff",
+    cardBgColor: "transparent",
+};
